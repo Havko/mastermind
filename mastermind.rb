45f3d2a9897e2feafd962b9cf806f30d2ed179d9
Steven Havko <stevenhavko@gmail.com>
2015-05-06 13:36:06 -0400

    Initial Commit

---
+++ mastermind.rb
@@ -0,0 +1,14 @@
+class Row
+  attr_accessor :value
+  def initialize(value = "")
+    @value = value
+  end
+end
+
+class CodePeg
+  attr_accessor :color
+    def initialize(color = "")
+      @color = color
+    end
+  end
+  
