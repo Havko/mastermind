a27606830a5d93825a12feb57e763581070598d2
Steven Havko <stevenhavko@gmail.com>
2015-05-06 16:25:18 -0400

    End of WorkDay

--- mastermind.rb
+++ mastermind.rb
@@ -1,14 +1,42 @@
-class Row
-  attr_accessor :value
-  def initialize(value = "")
-    @value = value
+
+
+class Code
+  attr_reader :code
+
+  def initialize(code = [])
+  end
+
+  def pick_code
+    colors = ["blue", "red", "green", "yellow", "orange", "purple"]
+    code = []
+    4.times do code.push(colors.sample)
+    end
+    @code = code
   end
 end
 
-class CodePeg
-  attr_accessor :color
-    def initialize(color = "")
-      @color = color
+class Turn
+  @@code = Code.new
+  @@code = @@code.pick_code
+  def player_move
+    puts @@code
+    puts "Please make your guess"
+    @guess = gets.chomp
+    @guess = @guess.split
+  end
+
+  def answer_check
+    if @guess == @@code
+      puts "Winner Winner Chicken Dinner!"
+    else
+      "Try Again"
     end
   end
-  
+end
+
+
+
+
+test = Turn.new
+puts test.player_move
+puts test.answer_check
